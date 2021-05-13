81c571f398bf1553d278c0ab0fc7aaec459dfad6
NadineBestard <nadine.bestard@ed.ac.uk>
2021-05-13 12:17:17 +0100

    try with sce, fail

--- src/old/generate_shiny.R
+++ src/old/generate_shiny.R
@@ -10,19 +10,20 @@ sce <- readRDS(here("processed", age, "sce_anno.RDS"))
  barcodes_mod <- paste0("1_", sce$Barcode)
  colnames(sce)<- make.unique(barcodes_mod)
 # # test with seurat
- seu <- as.Seurat(sce)
+ #seu <- as.Seurat(sce)
 
 
-conf <- createConfig(seu)
-# #Delete some unnecessary metadata
-# conf <- delMeta(conf, meta.to.del = c( "subsets_mt_sum", "subsets_mt_detected",
-#                        "outlier", "ratio_detected_sum", "outlier_ratio",
-#                        "discard", "sizeFactor", "total")
-#                 )
-# # Change name of some metadata
-# conf <- modMetaName(conf,
-#                     meta.to.mod = c("sum", "detected", "subsets_mt_percent", "celltype"),
-#                     new.name = c("umi counts", "detected genes", "% mt genes", "cell type"))
+conf <- createConfig(sce)
+#Delete some unnecessary metadata
+conf <- delMeta(conf, meta.to.del = c( "subsets_mt_sum", "subsets_mt_detected",
+                       "outlier", "ratio_detected_sum", "outlier_ratio",
+                       "discard", "sizeFactor", "total")
+                )
+# Change name of some metadata
+conf <- modMetaName(conf,
+                    meta.to.mod = c("sum", "detected", "subsets_mt_percent", "celltype"),
+                    new.name = c("umi counts", "detected genes", "% mt genes", "cell type"))
 # 
 
-makeShinyApp(seu, conf, gene.mapping = TRUE, shiny.title = "ShinyCell app")
+makeShinyApp(sce, conf, gene.mapping = TRUE, shiny.title = "ShinyCell app")
+
