6ccfa23c984911f70eb181d6a092e109aa30902f
NadineBestard <nadine.bestard@ed.ac.uk>
2021-06-29 15:18:24 +0100

    featureselection now 01

--- src/old/main.R
+++ src/old/main.R
@@ -10,9 +10,9 @@ rmarkdown::render(here("src", age, "QC_01.Rmd"))
 # normalisation
 rmarkdown::render(here("src", age, "normalise_01.Rmd"))
 #feature selection and dimensional reduction
-rmarkdown::render(here("src", age, "feature_selection_dimred.Rmd"))
+rmarkdown::render(here("src", age, "feature_selection_dimred_01.Rmd"))
 # clustering
-rmarkdown::render(here("src", age, "clustering.Rmd"))
+rmarkdown::render(here("src", age, "clustering_01.Rmd"))
 # annotation
 rmarkdown::render(here("src", age, "annotation.Rmd"))
 # cluster QC
