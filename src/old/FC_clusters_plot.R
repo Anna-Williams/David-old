6706b0ac0b423b15ad0fac592f427328e185d10b
NadineBestard <nadine.bestard@ed.ac.uk>
2021-10-08 15:36:54 +0100

    clean up and add comments

--- src/old/FC_clusters_plot.R
+++ src/old/FC_clusters_plot.R
@@ -1,43 +1,46 @@
+#SCRIPT FOR PLOT WITH FC ON Y AXIS, GROUPS IN X AXIS
+
+## set up ---
+
 library(here) #reproducible paths
 library(scater) #feature plots
 library(patchwork) # agregate plots
 library(pals) # for palettes with large n #kelly()22, #polychrome()#36, cols25()
 
+# project
 age <- "old"
 
+# colours
 # remove the black and white from the pallete, still 20 colours left
 kelly_col <- unname(kelly()[-c(1,2)])
 
-if (!file.exists(here("processed", age, "sce_anno_02.RDS"))) {
-  sce <- readRDS(here("processed", age, "sce_clusters_02.RDS"))
-}else{
-  sce <- readRDS(here("processed", age, "sce_anno_02.RDS"))
-}
-
-
-plotTSNE(sce, colour_by = "clusters_named", text_by = "clusters_named", text_size = 4) + scale_color_manual(values = kelly_col)
-
-
-## plot 
-
-# import data
+## import and transform data ---
+# this is the output from edgeR
+# a list with the DE results, each element named as one of the clusters and 
+# contain a DFrame with the DE for that cluster
 de_results <- readRDS(here("processed", age, "DE_k20_de_results.RDS"))
-# transform to df format
+
+# transform all the DFrame to a standart df format
 de_results_dfs <- lapply(de_results, 
                          function(x) {
                            na.omit(as.data.frame(x))
                          }
 )
-# merge all in a single dataframe
+# merge all the dfs from the list in a single big dataframe
 de_results_df <- bind_rows(de_results_dfs, .id = "cluster") %>% 
+  # only keep relevant columns
   select(cluster, logFC, FDR) %>% 
+  # add column that indicates if the result is significant
   mutate(Significant = ifelse(FDR < 0.05, 
                               yes = ifelse(logFC > 0, "upregulated", "downregulated"),
                               no = "not-significant")) %>% 
+  # order the levels to display in correct order in plot
   mutate(Significant = forcats::fct_relevel(Significant, c("upregulated", "downregulated", "not-significant"))) %>% 
   mutate(cluster = forcats::fct_relevel(cluster, c("Astrocyte_1", "Astrocyte_2", "Astrocyte_3", "OligoAstro", "Oligo_1", "Oligo_2", "OPCs", "mNeurons", "iNeurons & NRPs", "Lymphocytes", "BAMs", "DCs",  "Endothelial", "fEndothelia", "Mural_cells", "ChP_epithelial"))) %>% 
+  # sort so the significant values are plotted on top of the non significants
   arrange(desc(Significant))
 
+## plot ---
 ggplot(data = de_results_df, mapping = aes(x=cluster, y=logFC, color=Significant)) + 
   geom_jitter(size = 0.5) + 
   scale_colour_manual(values = c("darkred", "darkgreen", "#999999" )) +
